cd3170e42f49b381bd6e7defcd0a5d9d19cc492f
rober0 <115416432+rober0@users.noreply.github.com>
2025-05-13 20:42:20 -0300

    Add files via upload

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'dashboard'
 
@@ -7,3 +9,5 @@ urlpatterns = [
     path('pacientes/', views.pacientes, name='pacientes'),
     path('medicos/', views.medicos, name='medicos'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

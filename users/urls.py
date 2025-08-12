441b3351114c9a0e0c340d9bc7b20cac43fe66ca
RafaelAgren <rafael.martins.souza.agren@gmail.com>
2025-08-12 14:32:52 -0300

    sim

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('faleconosco/', views.contato_view, name="contact-us"),
 ]
